97445bf2bf11b17db0b610f372cf4ab372e92d5e
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-22 17:38:08 +0400

    add LayoutPage

---
+++ orelsa-fe/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Views/Landing/Header";
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
