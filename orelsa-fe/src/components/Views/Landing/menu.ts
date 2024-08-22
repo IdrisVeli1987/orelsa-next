63c0ee24786765778999e29099b253250fa3bf3a
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-22 17:39:04 +0400

    add navbar menu

---
+++ orelsa-fe/src/components/Views/Landing/menu.ts
@@ -0,0 +1,17 @@
+const menu = () => {
+  return [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "Shop",
+      link: "/shop",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
+};
+export default menu;
