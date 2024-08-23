a87a13c12885e11fd2963d4c34295e809ebf8e9b
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-23 11:30:02 +0400

    create HomePage

---
+++ orelsa-fe/src/app/(landing)/Home/page.tsx
@@ -0,0 +1,14 @@
+import LandingContainer from "@/components/Views/Landing/LandingContainer";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <main>
+      <LandingContainer>
+        <div className="bg-red-500">
+
+        </div>
+      </LandingContainer>
+    </main>
+  );
+}
