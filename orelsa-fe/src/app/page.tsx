2a479aaefd56dcb5c3d4cd238f447c6854bcac7b
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-23 20:35:13 +0400

    add HomePage to LandingPage

--- orelsa-fe/src/app/page.tsx
+++ orelsa-fe/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Views/Landing/Header";
 import React from "react";
+import HomePage from "./(landing)/Home/page";
 
 export default function LandingPage() {
   return (
     <>
       <Header />
+      <HomePage/>
     </>
   );
 }
