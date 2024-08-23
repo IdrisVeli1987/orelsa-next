51824e7759c4c6d19af1bbdfe0f2e9478eaa9b7a
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-23 20:39:49 +0400

    create db.ts file

---
+++ orelsa-fe/src/components/Views/Landing/db.ts
@@ -0,0 +1,24 @@
+export interface IRange {
+  id: string | number,
+  src: string,
+  desc: string
+}
+
+
+export const TheRangeData: any = [
+  {
+    id: 1,
+    src: "/TheRange/Group-1.svg",
+    desc: "Dining",
+  },
+  {
+    id: 2,
+    src: "/TheRange/Group-2.svg",
+    desc: "Living",
+  },
+  {
+    id: 3,
+    src: "/TheRange/Group-3.svg",
+    desc: "Bedroom",
+  },
+];
