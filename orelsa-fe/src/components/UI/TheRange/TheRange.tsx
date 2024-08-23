fc1ce8862d2f59a5cf2000a196e4cf3e46d3983b
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-23 20:39:36 +0400

    create TheRange component

---
+++ orelsa-fe/src/components/UI/TheRange/TheRange.tsx
@@ -0,0 +1,41 @@
+import { IRange, TheRangeData } from "@/components/Views/Landing/db";
+import LandingContainer from "@/components/Views/Landing/LandingContainer";
+import Image from "next/image";
+import React from "react";
+
+const TheRange: React.FC = () => {
+  return (
+    <section>
+      <LandingContainer>
+        <div className="flex flex-col justify-center items-center h-[810px]">
+          <div className="flex justify-center items-center mx-auto w-[400px] mb-4">
+            <h1 className="text-4xl font-bold text-[#333333;]">
+              Browse The Range
+            </h1>
+          </div>
+          <div className="text-center">
+            <p className="w-full">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+
+          <div className="flex pt-9 gap-6">
+            {TheRangeData.map(({ id, src, desc }: IRange) => {
+              return (
+                <div
+                  key={id}
+                  className="flex flex-col justify-center items-center"
+                >
+                  <Image src={src} alt="image" width={380} height={480} />
+                  <p className="mt-6 font-semibold text-2xl">{desc}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </LandingContainer>
+    </section>
+  );
+};
+
+export default TheRange;
