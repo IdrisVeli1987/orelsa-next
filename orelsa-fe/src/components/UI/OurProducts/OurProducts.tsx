73a000a9b72e7b4f4cda2af114e651c3343d08fc
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-25 16:51:25 +0400

    fix width

--- orelsa-fe/src/components/UI/OurProducts/OurProducts.tsx
+++ orelsa-fe/src/components/UI/OurProducts/OurProducts.tsx
@@ -11,12 +11,12 @@ const OurProducts: React.FC = () => {
             Our Products
           </h2>
 
-          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
             {TheOurProducts.map(({ id, src, subTitle, desc, price, sales }) => {
               return (
                 <div
                   key={id}
-                  className="flex flex-col justify-center items-center border rounded-lg shadow-md w-[285px] h-[446] mb-6 "
+                  className="flex flex-col justify-center items-center border rounded-lg shadow-md w-full h-[446] mb-6 "
                 >
                   <Image
                     src={src}
@@ -24,18 +24,20 @@ const OurProducts: React.FC = () => {
                     className="w-full h-48 object-cover rounded"
                     width={200}
                     height={200}
-                    style={{ width: "285px", height: "301px" }}
+                    style={{ width: "100", height: "301px" }}
                   />
-                  <div className="bg-[#F4F5F7] w-full h-full pb-4">
-                    <div className="px-4 py-6">
-                      <h2 className="text-2xl font-semibold px-4 py-6">
-                        {subTitle}
-                      </h2>
-                      <p className="text-gray-600 ">{desc}</p>
-                    </div>
-                    <div className="flex justify-between items-center gap-1 w-[285px]">
-                      <p className="text-sm font-bold ">${price}</p>
-                      {sales && <p className="text-[#B0B0B0]">Sale: {sales}</p>}
+                  <div className="bg-[#F4F5F7] w-full h-full pb-4 pl-4">
+                    {/* <div className="flex flex-col gap-2"> */}
+                    <h2 className="text-2xl font-semibold">{subTitle}</h2>
+                    <p className="text-gray-600">{desc}</p>
+                    {/* </div> */}
+                    <div className="flex items-center w-max gap-1 mt-2">
+                      <p className="text-xl font-bold">${price}</p>
+                      {sales && (
+                        <p className="text-[#B0B0B0] text-base">
+                          Sale: {sales}
+                        </p>
+                      )}
                     </div>
                   </div>
                 </div>
