a69c8358c9a0e670b66898cae9d75da360781cb1
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-27 15:26:25 +0400

    Import HomePage

--- orelsa-fe/src/app/page.tsx
+++ orelsa-fe/src/app/page.tsx
@@ -1,13 +1,12 @@
 import Header from "@/components/Views/Landing/Header";
 import React from "react";
-import HomePage from "./(landing)/Home/page";
+import HomePage from "./(landing)/page";
 
 export default function LandingPage() {
   return (
     <>
       <Header />
-      <HomePage/>
+      <HomePage />
     </>
   );
 }
-
