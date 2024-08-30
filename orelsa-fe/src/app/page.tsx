20045e968e29b33b8e4eae541509db125c3924da
IdrisVeli1987 <idrisveliev@gmail.com>
2024-08-30 16:03:08 +0400

    fix code

--- orelsa-fe/src/app/page.tsx
+++ orelsa-fe/src/app/page.tsx
@@ -1,15 +1,15 @@
 import Header from "@/components/Views/Landing/Header";
 import React from "react";
 import Slider from "@/components/UI/Slider/Slider";
-import HomePage from "./[locale]/(landing)/home/page";
-import ShopPage from "./[locale]/(landing)/shop/page";
+import HomePage from "./(landing)/home/page";
+import ShopPage from "./(landing)/shop/page";
 
 export default function LandingPage() {
   return (
     <>
       <Header />
       <HomePage />
-      <ShopPage/>
+      {/* <ShopPage/> */}
       <Slider />
     </>
   );
