050ba87f947846b65246073039c1a24d9a882aaa
IdrisVeli1987 <idrisveliev@gmail.com>
2024-09-02 20:04:35 +0400

    add Rights component

---
+++ orelsa-fe/src/components/UI/AllRights/Rights.tsx
@@ -0,0 +1,14 @@
+import LandingContainer from "@/components/Views/Landing/LandingContainer";
+import React from "react";
+
+const Rights = () => {
+  return (
+    <LandingContainer>
+      <div className="border-t-2 border-[#D9D9D9] w-full">
+        <p className="py-10">2024 . All rights reverved</p>
+      </div>
+    </LandingContainer>
+  );
+};
+
+export default Rights;
