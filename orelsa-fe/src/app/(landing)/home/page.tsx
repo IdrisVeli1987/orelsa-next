7dc763ad448bf41794a40081e7887df6a5890022
IdrisVeli1987 <idrisveliev@gmail.com>
2024-09-04 21:30:03 +0400

    fix code

--- orelsa-fe/src/app/(landing)/home/page.tsx
+++ orelsa-fe/src/app/(landing)/home/page.tsx
@@ -18,25 +18,32 @@ export default function HomePage() {
           className="w-full h-auto"
         />
 
-        <div className="bg-[#FFF3E3;] absolute top-1/2 right-[60px] transform -translate-x-1/2 -translate-y-1/2 z-10 p-4 w-[645px] h-[550px] mt-20">
-          <div className="w-[560px] h-[345px]">
-            <h5
-              id="ArruvalTitle"
-              className="text-[#333333] font-semibold text-base ml-11 mt-16"
-            >
-              New Arrival
-            </h5>
-            <h2 className="ml-11 mt-4 font-bold text-[52px] text-[#B88E2F] w-[12ch]">
-              Discover Our New Collection
-            </h2>
-            <p className="ml-11 mt-9 text-lg font-medium">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-              tellus, luctus nec ullamcorper mattis
-            </p>
-            <div className="ml-11 pb-10 pt-9">
-              <button className="gap-3 bg-[#B88E2F] w-[222px] h-[75px] font-bold text-base text-white">
-                BUY NOW
-              </button>
+        <div className="">
+          <div className="shadow-2xl border-none rounded-lg absolute top-1/2 right-[60px] transform -translate-y-1/2 z-10 p-4 w-[645px] h-[600px] mt-20">
+            <div className="flex justify-between w-10 ">
+              <div className="w-[560px] full">
+                <h5
+                  id="ArruvalTitle"
+                  className="text-[#333333] font-semibold text-base ml-11 mt-16"
+                >
+                  New Arrival
+                </h5>
+                <h2 className="ml-11 mt-4 font-bold text-[52px] text-[#B88E2F] w-[12ch]">
+                  Discover Our New Collection
+                </h2>
+                <p className="ml-11 mt-9 text-lg font-medium">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  elit tellus, luctus nec ullamcorper{" "}
+                  <span className="text-[#B88E2F] bg-transparent font-black  text-2xl ">
+                    mattis
+                  </span>
+                </p>
+                <div className="ml-11 pb-10 pt-9">
+                  <button className="gap-3 bg-[#B88E2F] w-[222px] h-[75px] font-bold text-base text-white">
+                    BUY NOW
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -45,7 +52,6 @@ export default function HomePage() {
       <OurProducts />
       <Rooms />
       <Footer />
-      
     </main>
   );
 }
