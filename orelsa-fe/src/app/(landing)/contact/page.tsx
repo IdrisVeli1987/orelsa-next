3552cdd860b1b605d6c4c18f184ec142f25050a0
IdrisVeli1987 <idrisveliev@gmail.com>
2024-09-07 20:43:39 +0400

    add Contact Page

--- orelsa-fe/src/app/(landing)/contact/page.tsx
+++ orelsa-fe/src/app/(landing)/contact/page.tsx
@@ -1,36 +1,39 @@
-import LandingContainer from "@/components/Views/Landing/LandingContainer";
+import { Form } from "@/components/UI/ContactForm/Form";
+import { FormTitle } from "@/components/UI/ContactForm/FormTitle";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Contact() {
   return (
-    <section className="bg-contact flex items-center justify-center min-h-[300px] w-full bg-no-repeat bg-center bg-cover">
-      <div className="flex flex-col justify-center items-center -mt-10">
-        <Image
-          src="/Contact/Logos-05.svg"
-          alt="Logo"
-          width={77}
-          height={77}
-          className="bg-no-repeat bg-center bg-cover"
-        />
+    <>
+      <section className="bg-contact flex items-center justify-center min-h-[300px] w-full bg-no-repeat bg-center bg-cover">
+        <div className="flex flex-col justify-center items-center -mt-10 w-full h-full">
+          <Image
+            src="/Contact/Logos-05.svg"
+            alt="Logo"
+            width={77}
+            height={77}
+            className="bg-no-repeat bg-center bg-cover"
+          />
 
-        <p className="text-5xl font-medium">Contact</p>
+          <p className="text-5xl font-medium">Contact</p>
 
-        <div className="flex justify-between items-center gap-3">
-          <Link href="/" className="text-base font-bold p-3">
-            Home
-          </Link>
+          <div className="flex justify-center items-center gap-3">
+            <Link href="/" className="text-base font-bold p-3">
+              Home
+            </Link>
 
-          <span className="font-bold">&gt;</span>
+            <span className="font-bold">&gt;</span>
 
-          <Link href="/shop" className="text-base font-medium">
-            Contact
-          </Link>
+            <Link href="/shop" className="text-base font-medium">
+              Contact
+            </Link>
+          </div>
         </div>
-      </div>
-      <LandingContainer>
-        {/* <Form /> */}
-      </LandingContainer>
-    </section>
+      </section>
+
+      <FormTitle />
+      <Form />
+    </>
   );
 }
