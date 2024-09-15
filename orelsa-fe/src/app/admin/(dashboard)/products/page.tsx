91bb98cc68ca80e00ea2dd94dae23b6785d46c77
IdrisVeli1987 <idrisveliev@gmail.com>
2024-09-15 09:29:35 +0400

    create AdminProducts

---
+++ orelsa-fe/src/app/admin/(dashboard)/products/page.tsx
@@ -0,0 +1,43 @@
+import AdminNewProduct from "@/components/Admin/AdminNewProduct";
+import AdminTable from "@/components/Admin/AdminTable";
+import AdminHeader from "@/components/Admin/Header";
+
+const AdminProducts = ({ searchParams: { add = "0" } }: any) => {
+  return (
+    <div>
+      <AdminHeader
+        title="Məhsul  paneli"
+        button={() => (
+          <>
+            <a
+              href="./products"
+              style={{
+                padding: 4,
+                borderRadius: 8,
+                backgroundColor: "#34C759",
+                color: "white",
+              }}
+            >
+              Məhsullara qayıt
+            </a>
+            <a
+              href="?add=1"
+              style={{
+                padding: 4,
+                borderRadius: 8,
+                backgroundColor: "#34C759",
+                color: "white",
+              }}
+            >
+              Yeni Mehsul
+            </a>
+          </>
+        )}
+      />
+      <AdminTable />
+      {add === "1" && <AdminNewProduct />}
+    </div>
+  );
+};
+
+export default AdminProducts;
