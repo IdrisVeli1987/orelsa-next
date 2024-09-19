5bac55d6a74906154d63ed59911a1c5711897896
IdrisVeli1987 <idrisveliev@gmail.com>
2024-09-19 20:42:17 +0400

    delete comments

--- orelsa-fe/src/app/admin/page.tsx
+++ orelsa-fe/src/app/admin/page.tsx
@@ -2,16 +2,12 @@ import LandingContainer from "@/components/Views/Landing/LandingContainer";
 import { Input } from "@nextui-org/react";
 
 const AdminDashboard = () => {
-  
   return (
     <section className="">
       <LandingContainer>
         <div className="w-svw h-svh flex items-center justify-center">
-          {/* Background styling */}
-          {/* Centered container with shadow and border */}
           <div className="shadow-2xl border-2 border-white-500/100 relative p-14 lg:w-[50%] mx-auto">
             <div className="w-full flex flex-col gap-4">
-              {/* Render input fields with different variants */}
               <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
                 <Input type="email" variant={"underlined"} label="User Name" />
               </div>
@@ -32,5 +28,3 @@ const AdminDashboard = () => {
 };
 
 export default AdminDashboard;
-
-
