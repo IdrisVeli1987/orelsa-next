db476b3228587fd00d538caae954da6fa8db2c5f
IdrisVeli1987 <idrisveliev@gmail.com>
2024-09-25 19:07:55 +0400

    add IoMdClose close button for SearchBar

--- orelsa-fe/src/components/Search/SearchBar.tsx
+++ orelsa-fe/src/components/Search/SearchBar.tsx
@@ -2,18 +2,12 @@
 import { cn } from "@nextui-org/theme";
 import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
-import {words} from "@/components/Search/db"
+import { words } from "@/components/Search/db";
+import { IoMdClose } from "react-icons/io";
 
 const SearchBar = () => {
   const [isSearching, setIsSearching] = useState(false);
   const [activeSearch, setActiveSearch] = useState([]);
-  
-//   const handleSearch = (e) => {
-//     if(e.target.value =="") {
-//         setActiveSearch([])
-//         return false
-//     }
-//     setActiveSearch(words.filter((w=> w.includes(e.target.value)).slice(0, 8) 
 
   const handleSearch = (e: any) => {
     e.preventDefault();
@@ -34,19 +28,20 @@ const SearchBar = () => {
           type="search"
           placeholder="Axtarış"
           className="w-full focus-visible:outline-none outline-none focus:outline-none h-full p-4 rounded-full bg-slate-200"
-        //   onChange={(e)=>handleSearch(e)}
         />
         <button
           onClick={handleSearch}
           className="absolute right-0 w-[50px] h-[50px] top-1/2 -translate-y-1/2 p-4 rounded-full bg-slate-300"
         >
-          <CiSearch />
+          {isSearching ? <IoMdClose /> : <CiSearch />}
         </button>
       </div>
 
-
-      
-      {/* <div className="absolute top-0 focus:top-20 p-4 bg-slate-200 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2"></div> */}
+      <div
+        className={`absolute ${
+          isSearching ? "top-20" : "top-0"
+        }p-4 bg-slate-200 text-white w-full rounded-xl left-1/2 -translate-x-1/2 flex flex-col gap-2`}
+      ></div>
     </form>
   );
 };
