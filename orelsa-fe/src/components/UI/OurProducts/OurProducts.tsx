cd552ea8d414eb04e11bf8217e5ab40f93a5be00
IdrisVeli1987 <idrisveliev@gmail.com>
2024-10-16 22:11:54 +0400

    fix our products in main page

--- orelsa-fe/src/components/UI/OurProducts/OurProducts.tsx
+++ orelsa-fe/src/components/UI/OurProducts/OurProducts.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import { getAllProducts } from "@/api/ui/requests";
 import ProductCard from "@/components/shared/ProductCard/ProductCard";
 import LandingContainer from "@/components/Views/Landing/LandingContainer";
 import { IProduct } from "@/interface/ui";
 import { Button } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
-const OurProducts: React.FC = async () => {
-  const TheOurProducts = await getAllProducts();
+const OurProducts: React.FC = () => {
+  const [showMore, setShowMore] = useState(false);
+  const [products, setNewProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await getAllProducts();
+        setNewProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
     <section>
@@ -16,21 +32,24 @@ const OurProducts: React.FC = async () => {
           </h2>
 
           <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-            {TheOurProducts.map((TheOurProduct: IProduct) => {
-              return (
-                TheOurProduct?.active && (
-                  <ProductCard key={TheOurProduct?._id} {...TheOurProduct} />
-                )
-              );
-            })}
+            {products
+              .slice(0, showMore ? products.length : 4)
+              .map((product: IProduct) => {
+                return (
+                  product?.active && (
+                    <ProductCard key={product?._id} {...product} />
+                  )
+                );
+              })}
           </div>
           <div className="w-full my-6 flex justify-center">
             <Button
               variant="solid"
               color="default"
               className="border-2 border-[#B88E2F] bg-white text-center py-8 px-16 font-semibold text-base text-[#B88E2F]"
+              onClick={() => setShowMore((pre) => !pre)}
             >
-              Show More
+              {showMore ? "Gizlət" : "Hamısına bax"}
             </Button>
           </div>
         </div>
