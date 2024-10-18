2bc079b45aa064247778466db11fbae29775b8b7
IdrisVeli1987 <idrisveliev@gmail.com>
2024-10-19 00:17:44 +0400

    add pagination to the products page of admin panel

--- orelsa-fe/src/components/Admin/AdminTable/index.tsx
+++ orelsa-fe/src/components/Admin/AdminTable/index.tsx
@@ -18,6 +18,7 @@ import { DeleteIcon } from "../../Icons/DeleteIcon";
 import { EditIcon } from "../../Icons/EditIcon";
 import { EyeIcon } from "../../Icons/EyeIcon";
 import { columns, users } from "./data";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 
 const statusColorMap: any = {
   active: "success",
@@ -44,6 +45,8 @@ interface IProduct {
 const AdminTable = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [editingId, setEditingId] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 15;
 
   const router = useRouter();
 
@@ -154,158 +157,208 @@ const AdminTable = () => {
     });
   };
 
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
   return (
-    <Table className="w-full">
-      <TableHeader columns={columns}>
-        {(column) => (
-          <TableColumn
-            key={column.uid}
-            align={column.uid === "actions" ? "center" : "start"}
-          >
-            {column.name}
-          </TableColumn>
-        )}
-      </TableHeader>
+    <div>
+      <Table className="w-full">
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === "actions" ? "center" : "start"}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
 
-      <TableBody items={users}>
-        {products?.map(
-          (
-            {
-              _id,
-              photos,
-              name,
-              description,
-              price,
-              discount,
-              discount_price,
-              model_no,
-              category,
-              active,
-            },
-            index: number
-          ) => {
-            return (
-              <TableRow key={_id}>
-                <TableCell>
-                  <Image src={photos[0]} alt={name} width={100} height={100} />
-                </TableCell>
-                <TableCell>
-                  <input
-                    className="bg-white"
-                    disabled={editingId !== _id}
-                    value={name}
-                    onChange={(e) => {
-                      handleChange(_id, "name", e.target.value);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <input
-                    className="bg-white"
-                    disabled={editingId !== _id}
-                    value={description}
-                    onChange={(e) => {
-                      handleChange(_id, "description", e.target.value);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <input
-                    className="bg-white"
-                    disabled={editingId !== _id}
-                    value={price}
-                    onChange={(e) => {
-                      handleChange(_id, "price", e.target.value);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <input
-                    className="bg-white"
-                    disabled={editingId !== _id}
-                    value={discount_price}
-                    onChange={(e) => {
-                      handleChange(_id, "discount_price", e.target.value);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <input
-                    className="bg-white"
-                    disabled={editingId !== _id}
-                    value={discount}
-                    onChange={(e) => {
-                      handleChange(_id, "discount", e.target.value);
-                    }}
-                  />
-                </TableCell>
+        <TableBody items={users}>
+          {currentItems?.map(
+            (
+              {
+                _id,
+                photos,
+                name,
+                description,
+                price,
+                discount,
+                discount_price,
+                model_no,
+                category,
+                active,
+              },
+              index: number
+            ) => {
+              return (
+                <TableRow key={_id}>
+                  <TableCell>
+                    <Image
+                      src={photos[0]}
+                      alt={name}
+                      width={100}
+                      height={100}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      className="bg-white"
+                      disabled={editingId !== _id}
+                      value={name}
+                      onChange={(e) => {
+                        handleChange(_id, "name", e.target.value);
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      className="bg-white"
+                      disabled={editingId !== _id}
+                      value={description}
+                      onChange={(e) => {
+                        handleChange(_id, "description", e.target.value);
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      className="bg-white"
+                      disabled={editingId !== _id}
+                      value={price}
+                      onChange={(e) => {
+                        handleChange(_id, "price", e.target.value);
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      className="bg-white"
+                      disabled={editingId !== _id}
+                      value={discount_price}
+                      onChange={(e) => {
+                        handleChange(_id, "discount_price", e.target.value);
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      className="bg-white"
+                      disabled={editingId !== _id}
+                      value={discount}
+                      onChange={(e) => {
+                        handleChange(_id, "discount", e.target.value);
+                      }}
+                    />
+                  </TableCell>
 
-                <TableCell>
-                  <input
-                    className="bg-white"
-                    disabled={editingId !== _id}
-                    value={model_no}
-                    onChange={(e) => {
-                      handleChange(_id, "model_no", e.target.value);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <input
-                    className="bg-white text-[#34C759]"
-                    disabled={editingId !== _id}
-                    value={category}
-                    onChange={(e) => {
-                      handleChange(_id, "category", e.target.value);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <input
-                    type="checkbox"
-                    className="bg-white"
-                    disabled={editingId !== _id}
-                    defaultChecked={active}
-                    onChange={(e: any) => {
-                      handleChange(_id, "active", e.target.checked);
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <div className="relative flex items-center gap-2 justify-center">
-                    <span className="text-lg cursor-pointer active:opacity-50 text-[#34C759]">
-                      {editingId === _id ? (
-                        <IoIosSave
+                  <TableCell>
+                    <input
+                      className="bg-white"
+                      disabled={editingId !== _id}
+                      value={model_no}
+                      onChange={(e) => {
+                        handleChange(_id, "model_no", e.target.value);
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      className="bg-white text-[#34C759]"
+                      disabled={editingId !== _id}
+                      value={category}
+                      onChange={(e) => {
+                        handleChange(_id, "category", e.target.value);
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <input
+                      type="checkbox"
+                      className="bg-white"
+                      disabled={editingId !== _id}
+                      defaultChecked={active}
+                      onChange={(e: any) => {
+                        handleChange(_id, "active", e.target.checked);
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <div className="relative flex items-center gap-2 justify-center">
+                      <span className="text-lg cursor-pointer active:opacity-50 text-[#34C759]">
+                        {editingId === _id ? (
+                          <IoIosSave
+                            onClick={() => {
+                              setEditingId(null);
+                              onSubmit(index);
+                            }}
+                          />
+                        ) : (
+                          <EditIcon onClick={() => setEditingId(_id)} />
+                        )}
+                      </span>
+                      <span
+                        className="text-lg text-danger cursor-pointer active:opacity-50 "
+                        onClick={() => handleDelete(_id)}
+                      >
+                        <DeleteIcon />
+                      </span>
+                      <span className="text-lg text-[#327ceb] cursor-pointer active:opacity-50">
+                        <EyeIcon
                           onClick={() => {
-                            setEditingId(null);
-                            onSubmit(index);
+                            router.push("/products/" + _id);
                           }}
                         />
-                      ) : (
-                        <EditIcon onClick={() => setEditingId(_id)} />
-                      )}
-                    </span>
-                    <span
-                      className="text-lg text-danger cursor-pointer active:opacity-50 "
-                      onClick={() => handleDelete(_id)}
-                    >
-                      <DeleteIcon />
-                    </span>
-                    <span className="text-lg text-[#327ceb] cursor-pointer active:opacity-50">
-                      <EyeIcon
-                        onClick={() => {
-                          router.push("/products/" + _id);
-                        }}
-                      />
-                    </span>
-                  </div>
-                </TableCell>
-              </TableRow>
-            );
-          }
-        )}
-      </TableBody>
-    </Table>
+                      </span>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              );
+            }
+          )}
+        </TableBody>
+      </Table>
+
+      {totalPages > 1 && (
+        <div className="flex justify-center mt-8">
+          <button
+            className="w-[40px] h-[40px] rounded-full flex justify-center items-center"
+            onClick={() => {
+              currentPage > 1 && setCurrentPage((prev) => prev - 1);
+            }}
+            disabled={currentPage === 1}
+          >
+            <GrFormPrevious />
+          </button>
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => paginate(index + 1)}
+              className={`w-[40px] h-[40px] rounded-full mx-2 ${
+                currentPage === index + 1
+                  ? "bg-green-500 text-white"
+                  : "bg-gray-200 text-gray-800"
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+          <button
+            className="w-[40px] h-[40px] rounded-full flex justify-center items-center"
+            onClick={() => {
+              currentPage < totalPages && setCurrentPage((prev) => prev + 1);
+            }}
+            disabled={currentPage === totalPages}
+          >
+            <GrFormNext />
+          </button>
+        </div>
+      )}
+    </div>
   );
 };
 
