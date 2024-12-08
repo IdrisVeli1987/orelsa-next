b514dbd808642d573ee8e570a70d275bc9e579bb
quliyevnamiq8@gmail.com <quliyevnamiq8@gmail.com>
2024-12-08 17:44:22 +0400

    dil deyisikliyi

--- orelsa-fe/src/components/UI/ContactForm/FormTitle.tsx
+++ orelsa-fe/src/components/UI/ContactForm/FormTitle.tsx
@@ -7,11 +7,10 @@ export const FormTitle = () => {
         <LandingContainer>
           <section className="">
             <div className="mt-24 w-full flex  items-center flex-col gap-2">
-              <h1 className="font-semibold text-4xl">Get In Touch With Us</h1>
+              <h1 className="font-semibold text-4xl">Bizimlə Əlaqə Saxlayın</h1>
               <p className="text-base font-normal text-center">
-                For More Information About Our Product & Services. Please Feel
-                Free To Drop Us <br /> An Email. Our Staff Always Be There To
-                Help You Out. Do Not Hesitate!
+              "Məhsul və xidmətlərimiz haqqında daha ətraflı məlumat üçün bizə e-poçt göndərə bilərsiniz.
+               <br /> Komandamız sizə kömək etmək üçün həmişə hazırdır. Çəkinmədən bizimlə əlaqə saxlayın!"              
               </p>
             </div>
           </section>
