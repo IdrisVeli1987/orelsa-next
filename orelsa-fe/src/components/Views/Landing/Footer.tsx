cb3dc077e0d5d6578b69c41ad783e19c107a8b4f
IdrisVeli1987 <idrisveliev@gmail.com>
2024-12-13 18:04:29 +0400

    change address in footer

--- orelsa-fe/src/components/Views/Landing/Footer.tsx
+++ orelsa-fe/src/components/Views/Landing/Footer.tsx
@@ -38,8 +38,7 @@ const Footer = () => {
           <div className="flex flex-col md:mb-0">
             <h3 className="text-2xl font-bold pb-[30px]">ORELSA COSMETİCS</h3>
             <p className="font-normal text-base">
-              400 University Drive Suite 200 Coral <br /> Gables, <br /> FL
-              33134 USA
+              Azərbaycan, Bakı
             </p>
           </div>
 
