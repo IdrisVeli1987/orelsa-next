badd97883b1020f190d415dea3216af609a65475
IdrisVeli1987 <idrisveliev@gmail.com>
2024-12-14 14:10:23 +0400

    fix issue (server comp to client comp)

--- orelsa-fe/src/app/(landing)/home/page.tsx
+++ orelsa-fe/src/app/(landing)/home/page.tsx
@@ -1,25 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
 import HomePageImg from "@/../public/HomePage/HomePageImg.svg";
 import OurProducts from "@/components/UI/OurProducts/OurProducts";
 import Rooms from "@/components/UI/Rooms/Rooms";
 import TheRange from "@/components/UI/TheRange/TheRange";
 import Footer from "@/components/Views/Landing/Footer";
 import { Button } from "@nextui-org/react";
-import axios from "axios";
 import Link from "next/link";
+import axios from "axios";
 
-export default async function HomePage() {
-  const { data } = await axios.get(
-    "http://localhost:9089/guest/homeNewCollection"
-  );
+export default function HomePage() {
+  const [data, setData] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:9089/guest/homeNewCollection"
+        );
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array means this will run only once on component mount
 
   const backgroundImage = data[0]?.newproductPhoto
     ? `url(${data[0].newproductPhoto})`
     : `url(${HomePageImg.src})`;
+
   return (
     <>
       <main
         className="bg-no-repeat bg-cover h-screen flex justify-end items-center"
-        style={{ backgroundImage: `${backgroundImage}` }}
+        style={{ backgroundImage: backgroundImage }}
       >
         <div className="w-full md:w-[90%] lg:w-[60%] xl:w-[50%] shadow-2xl bg-white bg-opacity-40 rounded-[16px] px-6 py-8 mx-5">
           <h2 className="ml-0 md:ml-11 mt-4 font-bold text-[32px] sm:text-[42px] lg:text-[52px] text-[#B88E2F]">
@@ -32,7 +51,7 @@ export default async function HomePage() {
             <Link
               href="https://wa.me/+994559706747"
               target="_blank"
-              className="gap-3 bg-[#B88E2F] absolute  w-full sm:w-[222px] h-[60px] sm:h-[75px] font-bold text-base text-hite"
+              className="gap-3 bg-[#B88E2F] absolute w-full sm:w-[222px] h-[60px] sm:h-[75px] font-bold text-base text-white"
             >
               <p className="text-center my-7">İndi sifariş et</p>
             </Link>
